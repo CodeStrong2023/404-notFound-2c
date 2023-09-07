a23ed197befc8d2982d7ab1049619176a7d295b6
Guillermo Giannone <guillegiannone@gmail.com>
2023-09-07 10:36:55 -0300

    Agrego Clase 4 parte 2 de Java

--- Guille-Giannone/java/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
+++ Guille-Giannone/java/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
@@ -1,6 +1,16 @@
-/
+
 package Clases;
 
 public class Persona {
+    //Atributos de la clase (Caracteristicas)
+    String nombre;
+    String apellido;
+    
+    //Metodo de la clase (Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+        
+    }
     
 }
