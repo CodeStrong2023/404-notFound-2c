01caeecf22bded73cbca4baac93f66be6cc5b8ba
Enzo <enzoastorga98@gmail.com>
2023-09-09 09:19:37 -0300

    Commit Clase 4 Java

---
+++ Enzo-Astorga/Java/Leccion4/Aritmética/src/Operaciones/Aritmética.java
@@ -0,0 +1,14 @@
+package Operaciones;
+
+public class Aritmética {
+    // Atributos de la clase
+    int a;
+    int b;
+    
+    //Método
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+}
