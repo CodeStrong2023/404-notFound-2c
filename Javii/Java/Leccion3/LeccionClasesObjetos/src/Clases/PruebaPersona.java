bd38522e05964cc052a23593910a534be4f860aa
Javier <javii07bj@gmail.com>
2023-09-25 20:26:39 -0300

    Agrego Leccion 3 a carpeta Java

---
+++ Javii/Java/Leccion3/LeccionClasesObjetos/src/Clases/PruebaPersona.java
@@ -0,0 +1,20 @@
+
+package Clases;
+
+public class PruebaPersona {
+	public static void main(String[] args) {
+		Persona persona1;
+		persona1 = new Persona();//Llamamos al constructor
+		persona1.nombre = "Ariel"; //El valor exadecimal normalmente comienza con 0x 
+		persona1.apellido = "Betancud";
+		persona1.obtenerInformacion();
+		
+		Persona persona2 = new Persona();
+		System.out.println("pesona2 = " + persona2);
+		System.out.println("pesona1 = " + persona1);
+		persona2.obtenerInformacion();
+		persona2.nombre = "Osvaldo";
+		persona2.apellido = "Giordanini";
+		persona2.obtenerInformacion();
+	}
+}
