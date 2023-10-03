b5a532a238617d77278f13e6ad710c42094e4a05
ignacio jose rocha <rochaignaciojose@gmail.com>
2023-10-02 23:54:43 -0300

    agrego la clase 7 de javascript

--- ignacio-rocha/javascript/Leccion01/02-01-ConcatenacionDeCadenas.js
+++ ignacio-rocha/javascript/Leccion01/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,21 @@ console.log(juntos);
 
 nombre+=apellido; //tercera concatenacion usando el operador simplificado
 console.log(nombre);
+
+//hoy ya no se usa var, se utiliza let y const
+let nombre2="Pedro";
+console.log(nombre2);
+
+const apellido2="Lepes";
+//apellido2="perez"; una constante no puede ser modificada
+console.log(apellido2);
+let x,y; //se pueden crear varias variables dentro de una misma linea
+x=17,y=21;//se puede hacer asignaciones de varias variables dentro de la misma linea
+let z=z=y;//se asigna el valor de la operacion
+console.log(z);
+
+let _1num=31;//no utilizar numeros para iniciar el nombre de una variable
+let rompienndo="rompe";//no utilizar palabras reservadas para variables
+
+console.log(_1num)
+console.log(rompienndo)
