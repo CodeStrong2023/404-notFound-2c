493f07b8d4f7fbf9946f93477f1e6b8418c4f425
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2023-10-05 16:08:00 -0300

    Agrego la clase_7 de JS

---
+++ Gonzalo-Balmaceda/JS/Leccion06/06-01-EjercioParesEImpares.js
@@ -0,0 +1,10 @@
+// Encontrar números paras e impares.
+const numero = 10;
+for (let i = 1; i <= numero; i++) {
+    if (i % 2 === 0) {
+        console.log(`pares = ${i}`);
+    } else {
+        console.log(`impares = ${i}`);
+    }
+}
+
---
+++ Gonzalo-Balmaceda/JS/Leccion06/06-02-EjercicioEsMayorDeEdad.js
@@ -0,0 +1,7 @@
+// Ejercicio: Es mayor de edad.
+const edad = 12;
+if (edad >= 18) {
+    console.log("Es mayor de edad");
+} else {
+    console.log("Es menor de edad");
+}
