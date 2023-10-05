56c98a9ab1213639af517d00d85c4892e557b846
Guillermo Giannone <guillegiannone@gmail.com>
2023-10-05 10:42:05 -0300

    Agrego clase 7 en JavaScript

---
+++ Guille-Giannone/Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+// Ejercicio para encontrar numeros pares e impares
+let parImpar = 12;
+if (parImpar % 2 == 0){
+    console.log("Es un numero PAR");
+}
+else{
+    console.log("Es un numero IMPAR")
+}
+
+//Ejercicio mayor de edad
+let edad = 18, adulto = 18;
+if(edad >= adulto){
+    console.log("Usted es una persona Adulta");
+}
+else{
+    console.log("Usted es una persona Menor de Edad")
+}
