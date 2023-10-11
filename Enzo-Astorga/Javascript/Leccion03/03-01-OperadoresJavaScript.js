45d8bc8a9d4027b63988839a70f4812d3de4ab4c
Enzo <enzoastorga98@gmail.com>
2023-10-11 20:59:45 -0300

    Agrego clase 7 en JavaScript

---
+++ Enzo-Astorga/Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,17 @@
+// Ejercicio para encontrar números pares e impares
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR.");
+}
+else{
+    console.log("Es un número IMPAR.")
+}
+
+// Ejercicio: Es mayor de edad
+let edad = 20, adulto = 18;
+if( edad >= adulto ){
+    console.log("Es MAYOR de edad.")
+}
+else{
+    console.log("Es MENOR de edad.")
+}
--- Enzo-Astorga/Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ Enzo-Astorga/Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos);
 
 nombre += apellido; // Tercera Concatenación usando el operador simplificado
 console.log(nombre);
+
+// Hoy ya no se utiliza var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+// apellido2 = "Peres"; una constante no puede ser modificada
+console.log(apellido2); // Console = Objeto, que llama al método o función .log y entre paréntesis se colocan los argumentos.
+
+let x, y; // Se pueden crear varias variables dentro de una misma línea
+x = 17, y = 21; // Se puede hacer asignación de varias variables dentro de una misma línea
+let z = x + y; // Se asigna el valor de la operación
+console.log(z);
+
+let _1num = 31; // No utilizar números para iniciar el nombre de una variable
+let rompiendo = "rompe"; // No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
