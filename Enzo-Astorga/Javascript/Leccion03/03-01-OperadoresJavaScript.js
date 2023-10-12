7a745a02d1adfca73776aa7f722940ef51fa33a6
Enzo <enzoastorga98@gmail.com>
2023-10-11 21:09:15 -0300

    Operadores en JavaScript y Ejercicios

--- Enzo-Astorga/Javascript/Leccion03/03-01-OperadoresJavaScript.js
+++ Enzo-Astorga/Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -15,3 +15,14 @@ if( edad >= adulto ){
 else{
     console.log("Es MENOR de edad.")
 }
+
+// Ejercicio: Dentro de un rango
+let dentroRango = 5; // Aquí vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+
+if( dentroRango >= valMin && dentroRango <= valMax ){
+    console.log("Está dentro del rango establecido.");
+}
+else{
+    console.log("Está fuera del rango establecido")
+}
