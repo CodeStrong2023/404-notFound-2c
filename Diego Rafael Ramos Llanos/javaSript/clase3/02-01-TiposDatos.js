9c77828b5e7eb532acfa718b953f51474bd40c21
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2023-10-13 18:59:40 -0300

    Se agrega la clase 3 de javascript(Leccion 2)

---
+++ Diego Rafael Ramos Llanos/javaSript/clase3/02-01-TiposDatos.js
@@ -0,0 +1,18 @@
+// Tipos de Datos en JavaScript
+/*
+La sintaxis en lo que es comentarios
+es muy similar a la de Java
+realmente diriamos que es identica.
+*/
+var nombre = "Diego"; //Tipo str
+console.log(nombre);
+ 
+var numero = "3000"; //Tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Diego",
+    apellido : "Rafael",
+    telefono : "12353524"
+}
+console.log(objeto);
