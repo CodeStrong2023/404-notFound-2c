4629c4f6c0ea2ae8a071bd936fc8dc631535af24
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2023-10-16 21:40:04 -0300

    se sube parte de la clase 7 de javaScript

---
+++ Diego Rafael Ramos Llanos/javaSript/clase7/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,35 @@
+var nombre = "Jose ";
+var apellido = "Montes ";
+var nombreCompleto = nombre + " " + apellido; 
+console.log(nombreCompleto);
+
+var nombreCompleto2 = "Diego " + "Rafael";
+console.log(nombreCompleto2);
+
+var juntos = nombre + 219; // Concatena el número con un Str.
+console.log(juntos);
+juntos = nombre + 78 + 17; 
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido; //Concatenación con el operador simplificado.
+console.log(nombre);
+
+let nombre2= "Pedro"
+console.log(nombre2) ;
+
+const apellid02="Lopes"
+//ape11id02 =Peres ;una constante no puede ser modificada
+consoLe.log(apellid02)
+
+let x,y;
+x = 17, y = 21;
+let z = x + y;
+consoLe.log(z)
+
+let Inum = 31;
+let rompiendo="rompe";
+
+console.log(Inum)
+console.log(rompiend)
