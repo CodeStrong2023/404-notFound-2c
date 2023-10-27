44b0ed320a79ae330101933d7bbac92cd32bc94a
Enzo <enzoastorga98@gmail.com>
2023-10-26 23:03:09 -0300

    Agrego Clase 11 de Java

---
+++ Enzo-Astorga/Java/Lección9/Arreglos_Ejercicio_2/src/arreglos_ejercicio_2/Arreglos_Ejercicio_2.java
@@ -0,0 +1,27 @@
+/*
+Ejercicio 2: Leer 5 números, guardarlos en un arreglo y
+mmostrarlos en el orden inverso al que sea han introducido.
+ */
+package arreglos_ejercicio_2;
+
+import java.util.Scanner;
+
+public class Arreglos_Ejercicio_2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        float numeros[] = new float[5];
+        
+        System.out.println("Guardando los elementos del arreglo");
+        for(int i = 0; i < 5; i++) {
+            System.out.println((i+1)+". Digite un número: ");
+            numeros[i] = entrada.nextFloat();
+        }
+        
+        System.out.println("\nImprimimos los elementos del arreglo");
+        for(int i = 4; i >= 0; i--){
+            System.out.println(" "+numeros[i]);
+        }
+        System.out.println("\n");
+    }
+    
+}
