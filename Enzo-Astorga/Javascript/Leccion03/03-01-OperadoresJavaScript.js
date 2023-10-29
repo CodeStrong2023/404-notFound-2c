83b6022457d3d2c28af3505c61cf20d75f5efbe6
Enzo <enzoastorga98@gmail.com>
2023-10-28 23:10:06 -0300

    Agrego clase 9 de JavaScript

--- Enzo-Astorga/Javascript/Leccion03/03-01-OperadoresJavaScript.js
+++ Enzo-Astorga/Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -24,5 +24,45 @@ if( dentroRango >= valMin && dentroRango <= valMax ){
     console.log("Está dentro del rango establecido.");
 }
 else{
-    console.log("Está fuera del rango establecido")
+    console.log("Está fuera del rango establecido");
 }
+
+// Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo");
+}
+else{
+    console.log("El padre No puede asistir al juego de su hijo");
+}
+
+// Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2);
+let numero = 12;
+resultado2 = numero % 2 == 0 ? "Es un número PAR" : "Es un número IMPAR";
+console.log(resultado2);
+
+//Convertir String a Number
+let miNumero = "21"; // Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); // Esta es una función
+console.log(typeof edad2);
+// Función isNaN (Para verificar que el valor de una variable sea de tipo numérica)
+if(isNaN(edad2)){ // No es un número = is Not a Number(devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo números")
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar");
+    }
+    else{
+        console.log("Muy jóven para votar");
+    }
+}
+
+
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy jóven para votar";
+console.log(resultado3);
+
+
