d14353db8cef58885643e432afa5399dc8537549
Guillermo Giannone <guillegiannone@gmail.com>
2023-11-01 22:38:16 -0300

    Agrego clase 10 JS

--- Guille-Giannone/Javascript/Leccion04/04-01-Ejercicios.js
+++ Guille-Giannone/Javascript/Leccion04/04-01-Ejercicios.js
@@ -44,3 +44,70 @@ const fechaNacimiento = 2006;
 console.log(fechaNacimiento);
 //fechaNacimiento = 2003;
 //console.log(fechaNacimiento);// Solo se ejecuta el console anterior
+
+
+//Ejercicio 1: Calcular la estacion del año
+let mes = 12;
+let estacion;
+if(mes == 1 || mes == 2 || mes == 12){
+    estacion = "Verano";
+} 
+else if (mes == 3 || mes == 4 || mes == 5){
+    estacion = "Otoño"
+}
+else if (mes == 6 || mes == 7 || mes == 8){
+    estacion = "Invierno"
+}
+else if (mes == 9 || mes == 10 || mes == 11){
+    estacion = "Primavera"
+}
+else{
+    estacion = "Valor Incorrecto"
+}
+console.log("El valor corresponde a la estacion: "+estacion);
+
+//Ejercicio 2: Hora del dia
+/*
+de 6 a 11 nos saluda: Good Morning
+de 12 a 16 nos saluda: Good Afternoom
+de 17 a 19 nos saluda: Good evening
+de 20 a 23 nos saluda: Good night
+Trabajaremos con 24 hs
+*/
+let horaDia = 7;
+let mensaje;
+if(horaDia >= 6 && horaDia <= 11){
+    mensaje = "Good Morning"
+}
+else if(horaDia >= 12 && horaDia <= 16){
+    mensaje = "Good Afternoom"
+}
+else if(horaDia >= 17 && horaDia <= 19){
+    mensaje = "Good Evening"
+}
+else if(horaDia >= 20 && horaDia <= 23){
+    mensaje = "Good Night"
+}
+else{
+    mensaje = "Valor incorrecto"
+}
+console.log(mensaje)
+
+//Estructura Swith (la sintaxis es igual a java)
+switch(mes){ // no solo se puede usar numero sino tambien cadena
+    case 1: case 2: case 12:
+        estacion = "Verano"
+        break;
+    case 3: case 4: case 5:
+        estacion = "Otoño"
+        break;
+    case 6: case 7: case 8:
+        estacion = "Invierno"
+        break;
+    case 9: case 10: case 11:
+        estacion = "Primavera"
+        break;
+    default:
+        estacion = "Valor incorrecto";
+}
+console.log("Bienvenido a la estacion de: "+estacion);
