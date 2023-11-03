bd3203a2844d1a9d5a836e7cea33decab427acd0
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2023-11-03 01:01:10 -0300

    Create Ejercicio Colecciones03.py

---
+++ Pulitta Celeste/Ejercicio Colecciones03.py
@@ -0,0 +1,28 @@
+# Ejercicio 3: Agregar personajes a una lista
+# Escriba un programa donde cree una lista con los siguientes personajes del señor de los anillos
+# Nombre: Aragon
+# Clase: Guerrero
+# Raza: Dúnadan del norte
+
+# Nombre: Gandalf
+# Clase: Mago
+# Raza: Istar
+
+# Nombre: Legolas
+# Clase: Arquero
+# Raza: Elfo Sindar
+
+personajes = []
+personaje1 = {'Nombre': 'Aragon', 'Clase': 'Guerrero', 'Raza': 'Dúnadan del norte'}
+personajes.append(personaje1)
+personaje2 = {'Nombre': 'Grandalf', 'Clase': 'Mago', 'Raza': 'Istar'}
+personajes.append(personaje2)
+personaje3 = {'Nombre': 'Legolas', 'Clase': 'Arquero', 'Raza': 'Elfo sindar'}
+personajes.append(personaje3)
+personaje4 = {'Nombre': 'Frodo', 'Clase': 'Arquero', 'Raza': 'Hobbit'}
+personajes.append(personaje4)
+personaje5 = {'Nombre': 'Gimli', 'Clase': 'Guerrero', 'Raza': 'Enano'}
+personajes.append(personaje5)
+personaje6 = {'Nombre': 'Galadriel', 'Clase': 'Sabia', 'Raza': 'Elfo de El Silmarillion'}
+personajes.append(personaje6)
+print(personajes)
