937431298b415b7d22af040d315dc8b80cdc1c46
Javier <javii07bj@gmail.com>
2023-11-07 15:53:08 -0300

    Agrego clase 8 Java

--- Javii/Java/Leccion5/Encapsulamiento/src/dominio/Persona.java
+++ Javii/Java/Leccion5/Encapsulamiento/src/dominio/Persona.java
@@ -38,5 +38,9 @@ public class Persona {
 		this.eliminado = eliminado;
 	}
 	
-	
+	public String toString(){ // Convierte en una cadena cada atributo
+		return "Persona [ nombre: "+this.nombre+
+			",sueldo: "+this.sueldo+
+			",eliminado: "+this.eliminado+" ]";
+	}
 }
--- Javii/Java/Leccion5/Encapsulamiento/src/test/PersonaPruba.java
+++ Javii/Java/Leccion5/Encapsulamiento/src/test/PersonaPruba.java
@@ -6,6 +6,7 @@ import dominio.Persona;
 public class PersonaPruba {
 	public static void main(String[] args) {
 		Persona persona1 = new Persona("Osvaldo", 57.000, false);
+		System.out.println("persona1 = " + persona1);
 		//Modificar a travez de los metodos
 		persona1.setNombre("Juan Ignacio");
 		//System.out.println("Nombre es: "+persona1.nombre);// error
@@ -13,6 +14,8 @@ public class PersonaPruba {
 		System.out.println("persona1 con su nombre modificado es: "+persona1.getNombre());
 		System.out.println("persona1 el resultado para el sueldo: "+persona1.getSueldo());
 		System.out.println("persona1 para obtener el booleano: "+persona1.isEliminado());
+		
+		System.out.println("persona1: "+persona1.toString());
 		
 	}
 }
