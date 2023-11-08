eae164d20bd84a36790191bca24279f7f8454b35
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2023-11-08 13:31:19 -0300

    Se sube parte de la clase 11 de python

---
+++ Diego Rafael Ramos Llanos/python/leccion 11/Vehiculo.py
@@ -0,0 +1,19 @@
+class vehiculo:
+    ''' Definir una clase padre llamada vehiculo y dos clase hijas llamdas
+    auto y vicicleta, las cuales heredan la clase padre vehiculo.La clase padre
+    debe tener los siguientes atributos y metodos:
+
+    Vehiculo (clase padre)
+    -Atributos(color, ruedas)
+    -Métodos(__init__(color,ruedas) y __str__())
+
+    Auto(clase hija de Vehiculo)
+    -Atributos(velocidad (km/hr))
+    -Métodos(__init__(color,ruedas,velocidad) y __str__() )
+
+    Bicicleta(clase de Vehiculo)
+    -Atributos (tipo(urbana/montaña/etc.)
+    -Métodos(__init__(color,ruedas,tipo) y __ str__()
+
+    Crear un objeto de cada clase
+    '''
