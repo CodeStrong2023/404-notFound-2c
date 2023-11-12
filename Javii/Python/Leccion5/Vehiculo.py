c8a6084064b6f3c36a2821ec254554c0f48f1595
Javier <javii07bj@gmail.com>
2023-11-11 21:43:14 -0300

    Agrgo Clase 11 de Python

---
+++ Javii/Python/Leccion5/Vehiculo.py
@@ -0,0 +1,59 @@
+class Vehiculo:
+
+
+#Definir una clase padre llamada Vehículo y dos clases hijas llamadas
+#Auto y Bicicleta, las cuales se heredan de la clase padre Vehículo. La clase
+#padre debe tener los siguientes atributos y métodos:
+
+#Vehículo (clase padre)
+#-Atributos(color, ruedas)
+#-Métodos(__init__(color, ruedas) y __str__())
+
+#Auto(clase hija de Vehículo)
+#-Atributos(velocidad (km/hr))
+#-Métodos(__init__(color, ruedas, velocidad)) y (__str__())
+
+#Bicicleta(clase hija de Vehículo)
+#-Atributos(tipo(urbana/montaña/etc.))
+#-Métodos(__init__(color, ruedas, tipo) y __str__())
+
+#Crear un objeto de cada clase
+
+
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return "Color: " + self.color + ", Ruedas: " + str(self.ruedas)
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__() + ", Velocidad(km/hr): " + str(self.velocidad)
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__() + ", Tipo: " + self.tipo
+
+
+# Primer objeto de la clase padre Vehículo
+vehiculo = Vehiculo("Blanco", 4)s
+print(vehiculo)
+
+# Segundo objeto pero ahora de la clase auto
+auto = Auto("Amarillo", 4, 120)
+print(auto)
+
+# Tercer objeto, el último de la clase Bicicleta
+bici = Bicicleta("Azul", 2, "Urbana")
+print(bici)
